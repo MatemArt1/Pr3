39dec7ae6857c6773aeb08295caa756421d5fb32
Matemart1 <matemart1@ya.ru>
2022-09-23 14:08:30 +0300

    Add class in src

---
+++ src/Like3.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.List;
+    public class Like3 {
+        public static boolean  isIsogram(String str) {
+            String newStr = str.toLowerCase();
+            List<Boolean>comparisons = new ArrayList<Boolean>();
+            int len = newStr.length();
+            for(int i = 0; i<len; i++ ){
+                for(int n = 0; n<len; n++){
+                    if(i!=n){
+                        boolean comparison = newStr.charAt(i)==newStr.charAt(n);
+                        comparisons.add(comparison);
+                    }
+                }
+            }
+            if(comparisons.contains(true)){
+                return false;
+            } else{
+                return true;
+            }
+        }
+    }
+
